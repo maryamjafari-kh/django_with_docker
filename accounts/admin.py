361ad953a16500c2033d5f4ad2c73622066dfd5e
maryam <jafari2000@gmail.com>
2024-03-30 15:48:52 +0330

    add home page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserChangeForm,CustomUserCreationForm
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model=CustomUser
+    add_form=CustomUserCreationForm
+    form=CustomUserChangeForm
+    list_display=('email','username')
+    
